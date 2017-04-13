adf3dc7035a9ec76811c07a63d1c748fbe1d9e5f
kim <315197141@qq.com>
2017-04-14 00:41:38 +0800

    HomeFt的部署

--- LoL-master/MyLOL_Project/app/src/main/java/com/mylol/project/ui/fragment/HomeFt.java
+++ LoL-master/MyLOL_Project/app/src/main/java/com/mylol/project/ui/fragment/HomeFt.java
@@ -1,24 +1,58 @@
 package com.mylol.project.ui.fragment;
 
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-
 import com.it.administrator.mylol_project.R;
+import com.mylol.project.Config;
+import com.mylol.project.base.BaseFragment;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
- * Created by Administrator on 2017/4/10.
+ * Created by kim
+ * on 2017/4/10.
  */
-public class HomeFt extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+public class HomeFt extends BaseFragment {
+    boolean connected;
+    private List<Integer> images;//滑动广告的图片地址
 
-        View view = inflater.inflate(R.layout.homeft_fragment_layout, null);
-        return view;
+    @Override
+    public int getLayoutId() {
+        return R.layout.homeft_fragment_layout;
     }
+
+    @Override
+    public void initVariables() {
+        checkNet();
+        images = new ArrayList<>();
+        images = Arrays.asList(Config.IMAGE);
+
+    }
+
+    /**
+     * 检查网络
+     */
+    private void checkNet() {
+        ConnectivityManager manager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = manager.getActiveNetworkInfo();
+        connected = info != null && info.isConnected();
+
+    }
+
+    @Override
+    public void initViews(View view, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void initLoadingDatas() {
+
+    }
+
+
 }
